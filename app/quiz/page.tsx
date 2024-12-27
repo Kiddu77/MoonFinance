21fff7e2d753e95939523ba6b6c0e2e856f77a0b
Soumil.Binhani <107888098+SoumilB7@users.noreply.github.com>
2024-12-27 10:53:11 +0530

    Update page.tsx

--- app/quiz/page.tsx
+++ app/quiz/page.tsx
@@ -1,7 +1,7 @@
 "use client";
 
-// import React, { useEffect, useState } from "react";
-import { useRouter } from "next/navigation";
+import React, { useEffect, useState } from "react";
+// import { useRouter } from "next/navigation";
 // import Header from "@/app/components/Header";
 import { getCookie } from "cookies-next";
 
